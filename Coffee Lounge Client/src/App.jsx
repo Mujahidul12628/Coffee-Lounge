85df66a91c51e701077803867cf8f96996fac455
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-10-30 14:55:57 +0600

    Created data and send it to mongodb

--- Coffee Lounge Client/src/App.jsx
+++ Coffee Lounge Client/src/App.jsx
@@ -1,26 +1,27 @@
 
-import { useLoaderData } from 'react-router-dom'
+// import { useLoaderData } from 'react-router-dom'
 import './App.css'
-import CoffeeCard from './components/CoffeeCard';
-import { useState } from 'react';
+// import CoffeeCard from './components/CoffeeCard';
+// import { useState } from 'react';
 
 function App() {
 
-  const loadedCoffees = useLoaderData();
-  const [coffees, setCoffees] = useState(loadedCoffees);
+  // const loadedCoffees = useLoaderData();
+  // const [coffees, setCoffees] = useState(loadedCoffees);
 
   return (
     <div className='m-20'>
-      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
-      <div className='grid md:grid-cols-2 gap-4'>
-        {
+      {/* <h1 className='my-20 text-6xl text-center text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1> */}
+      <div className='grid gap-4 md:grid-cols-2'>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, suscipit?</p>
+        {/* {
           coffees.map(coffee => <CoffeeCard
             key={coffee._id}
             coffee={coffee}
             coffees={coffees}
             setCoffees={setCoffees}
           ></CoffeeCard>)
-        }
+        } */}
       </div>
     </div>
   )
